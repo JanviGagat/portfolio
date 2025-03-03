7f3dacd1b9a9f8f5373c6836bd82f46f3edca544
JanviGagat <janvigagat19@gmail.com>
2025-03-03 16:05:19 +0530

    feat initial file

---
+++ public/data.js
@@ -0,0 +1,34 @@
+let skillArr = [
+  {
+    name: "Python",
+    src: "./public/python.jpeg",
+  },
+  {
+    name: "Python",
+    src: "./public/python.jpeg",
+  },
+  {
+    name: "Python",
+    src: "./public/python.jpeg",
+  },
+  {
+    name: "Python",
+    src: "./public/python.jpeg",
+  },
+  {
+    name: "Python",
+    src: "./public/python.jpeg",
+  },
+  {
+    name: "Python",
+    src: "./public/python.jpeg",
+  },
+  {
+    name: "Python",
+    src: "./public/python.jpeg",
+  },
+  {
+    name: "",
+    src: "",
+  },
+];
